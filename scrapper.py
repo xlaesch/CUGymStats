6bf089251b1f7c2199b663f2ff08992fa309c7c0
Alexander Schneider <alex@alexsch.dev>
2025-01-11 16:54:44 -0500

    implement HTML parser and parsed relevant data (lastcount and percent)

--- scrapper.py
+++ scrapper.py
@@ -1,13 +1,31 @@
 import requests
 from fake_useragent import UserAgent
-
-#TODO: add user-agent header to bypass mod_security
-r = requests.get("https://connect2concepts.com/connect2/?type=circle&key=355de24d-d0e4-4262-ae97-bc0c78b92839&loc_status=false")
-
-print(r) #gives status code of response received
-
-print(r.content)
+from bs4 import BeautifulSoup
 
 
-ua = UserAgent()
-print(ua.random)
+ua = UserAgent() #to bypass Mod_Security --> bot prevention
+
+headers = {
+    'User-Agent' : ua.random
+}
+
+r = requests.get("https://connect2concepts.com/connect2/?type=circle&key=355de24d-d0e4-4262-ae97-bc0c78b92839&loc_status=false", headers=headers)
+
+print(r) #gives status code of response received, 200 --> successful response
+
+#Parse the HTML
+soup = BeautifulSoup(r.content, 'html.parser')
+
+#Helen Newman Data Extraction
+helen_newman_data = soup.select_one('.col-md-3.col-sm-6 .circleChart')
+print(helen_newman_data)
+
+if helen_newman_data:
+    data_lastcount = helen_newman_data.get('data-lastcount')
+    data_percent = helen_newman_data.get('data-percent')
+else:
+    print('Element not found.')    
+
+print(data_lastcount,data_percent)
+
+    
