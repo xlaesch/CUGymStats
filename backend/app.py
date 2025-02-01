79184da5e6d00f64a33206c4c5fdf6d8589937c6
Alexander Schneider <alex@alexsch.dev>
2025-02-01 17:17:04 -0500

    Refactor app.py to remove commented-out table initialization logic and directly insert scraped data into the database

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 import sys
-from database.db_helper import init_table
+from database.db_helper import init_table, insert_new_data
 from scraper.scraper import *
 
 scraped_data = parse_html_with_selenium()
 
-if sys.argv[1]: #Checking to see if it's the first time running and to see if tables have to be initialized
+'''if sys.argv[1] == '-s':  # Checking to see if it's the first time running and if tables need to be initialized
     data_names = list(scraped_data.keys())
     for name in data_names:
-        init_table(name)
+        init_table(name)''' #TODO: Fix!
+
+insert_new_data(scraped_data)
 
